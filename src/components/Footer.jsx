57c2397db5fc5aedf14665410475cd38cb384d79
pablo cannizzo <pablocannizzo@gmail.com>
2023-09-26 09:45:38 -0300

    primer commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <>
+            <p>@Copyright 2023 - Cannizzo Pablo</p>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+const Header = () => {
+
+    return (
+        <>
+            <nav className="navbar">
+                <div id="navbarContenido" className="navbarContenido">
+                    <ul>
+                        <li>
+                            <a href="./index.html">
+                                <h1>Mi proyecto React</h1>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">productos</a>
+                        </li>
+                        <li>
+                            <a href="#">Envios</a>
+                        </li>
+                        <form role="search">
+                            <input type="search" placeholder="Search" aria-label="Search"></input>
+                            <button type="submit">Buscar</button>
+                        </form>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header
